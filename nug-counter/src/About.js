d43d0c0a20981ebe458da4b47b2305ab7283e53b
JakeBalakrishna <jake.balakrishna@aaoglobal.com>
2023-01-26 11:09:25 +0000

    nug-counter

--- nug-counter/src/About.js
+++ nug-counter/src/About.js
@@ -1,30 +1,45 @@
-import React, {useState} from 'react'
-import './style.css'
-import './images/FallingNugget3-1.jpg'
+
+import React, { useState } from "react";
+import "./style.css";
+import chickenNuggi from './images/chickenNuggi.jpg';
+import hamGurber from './images/hamGurber.jpg';
 
 export default function About() {
-    const [count, setCount] = useState(0);
-    
+  const [count, setCount] = useState(0);
+  const [image, setImage] = useState(chickenNuggi);
+  const [calories, setCalories] = useState(59);
 
-    const increment = ()  => {
-    setCount(count + 1)
-    };
-    
-    return(
-    <>   
+  const increment = () => {
+    setCount(count + 1);
+  };
 
-        <div>
-        <img src={require('./images/FallingNugget3-1.jpg')} className='avatar' alt='A sinlge chicken nugget' />
+  const changeImage = (img, cal) => {
+    setImage(img);
+    setCalories(cal);
+  };
+
+  return (
+    <>
+      <div>
+        <div className='nav'>
+            <div className='nav-butt'>
+                <button onClick={() => changeImage(chickenNuggi, 59)}>Chicken nuggi</button>
+                <button onClick={() => changeImage(hamGurber, 257)}>Ham Gurger</button>
+            </div>
         </div>
-        
-        <div className="nug-counter">
-            <p>Nug counter: {count}</p>
-            <p>Calories: {count * 59}</p>
-             <button onClick={increment} >I can have one more</button>      
-        </div>
-        
+
+        <img
+          src={image}
+          className="avatar"
+          alt="A sinlge chicken nugget"
+        />
+      </div>
+
+      <div className="nug-counter">
+        <p>counter: {count}</p>
+        <p>Calories: {calories * count}</p>
+        <button onClick={increment}>I can have one more</button>
+      </div>
     </>
-        
-        
-    )
+  );
 }
